b2c629b691ae460940f2091f3cd0bd6fcce7f9e6
Shinae0401 <qkqjzz486@naver.com>
2023-04-18 11:46:32 +0900

    세팅

---
+++ src/main/java/com/example/project_ciku/ProjectCikuApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_ciku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCikuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_ciku/ProjectCikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_ciku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
